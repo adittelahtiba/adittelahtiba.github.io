f88d6ec2dfae6255733388e7756ebfcac817371a
Aditya Pangestu <pangestuaditya.solihin@gmail.com>
2021-03-27 19:38:55 +0700

    preview rombak

--- assets/js/undangan.js
+++ assets/js/undangan.js
@@ -1,10 +1,28 @@
 // .
 let videooffline = document.getElementById('videooffline');
+$('.pause').hide();
+$('.off-sound').hide();
 
 function play() {
-		videooffline.play();
+	videooffline.play();
+	$('.play').hide();
+	$('.pause').show();
 }
 
 function pause() {
-		videooffline.pause();
+	videooffline.pause();
+	$('.pause').hide();
+	$('.play').show();
+}
+
+function onsound() {
+	// videooffline.play();
+	$('.on-sound').hide();
+	$('.off-sound').show();
+}
+
+function offsound() {
+	// videooffline.pause();
+	$('.off-sound').hide();
+	$('.on-sound').show();
 }

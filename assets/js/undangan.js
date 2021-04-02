31ca54e1cc12ea4925bd5c0ec1b8e0286b9d63aa
Aditya Pangestu <pangestuaditya.solihin@gmail.com>
2021-04-02 10:26:09 +0700

    countdown good

--- assets/js/undangan.js
+++ assets/js/undangan.js
@@ -1,21 +1,73 @@
 $(window).on('load', function () {
 
-let videooffline = document.getElementById('videooffline');
-let audiona = document.getElementById('audiona');
-$('.pause').hide();
-$('.off-sound').hide();
-offsound();
+	let videooffline = document.getElementById('videooffline');
+	let audiona = document.getElementById('audiona');
+	$('.pause').hide();
+	$('.off-sound').hide();
+	offsound();
+
+	setInterval(function () {
+		hitungmundur();
+	}, 1000);
 
 
-  //countdown
-  if ($('.countdown').length) {
-    var count = $('.countdown').data('count');
-    var template = $('.countdown').html();
-    $('.countdown').countdown(count, function (event) {
-      $(this).html(event.strftime(template));
-    });
-  }
-  
+	//countdown
+	function hitungmundur() {
+		var endTime = new Date($('.countdown')[0].dataset.count);
+		endTime = (Date.parse(endTime) / 1000);
+
+		var now = new Date();
+		now = (Date.parse(now) / 1000);
+
+		var timeLeft = endTime - now;
+
+		var days = Math.floor(timeLeft / 86400);
+		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+		if (hours < "10") {
+			hours = "0" + hours;
+		}
+		if (minutes < "10") {
+			minutes = "0" + minutes;
+		}
+		if (seconds < "10") {
+			seconds = "0" + seconds;
+		}
+
+		if ($('.countdown').length) {
+			$('.countdown').html(`<div class="box-ctdn text-center">
+		<p>
+			${days}
+		</p>
+		<small>Hari</small>
+	</div>
+	<div class="box-ctdn text-center">
+		<p>
+			${hours}
+		</p>
+		<small>Jam</small>
+	</div>
+	<div class="box-ctdn text-center">
+		<p>
+			${minutes}
+		</p>
+		<small>Menit</small>
+	</div>
+	<div class="box-ctdn text-center">
+		<p>
+			${seconds}
+		</p>
+		<small>Detik</small>
+	</div>`);
+		}
+	}
+
+	$('#live-btn').on('click', () => {
+		$('#live-frame').show();
+	});
+
 });
 
 function play() {
@@ -42,11 +94,10 @@ function offsound() {
 	$('.on-sound').show();
 }
 
-function navhide(){
+function navhide() {
 	$('.navbar').hide();
 }
 
-$('.modal').on('hidden.bs.modal', function() {
+$('.modal').on('hidden.bs.modal', function () {
 	$('.navbar').show();
 });
-

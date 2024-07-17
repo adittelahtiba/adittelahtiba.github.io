8c04b88ae440d9d2af23d129681c1881708ccddf
adit <pangestuaditya.solihin@gmail.com>
2024-07-17 14:48:00 +0700

    kin

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -18,3 +18,29 @@ function getCurrentYear() {
 }
 
 getCurrentYear();
+
+
+// Function to fetch and display JSON data
+function fetchData() {
+    fetch('/json/data.json') // Replace with your JSON data URL
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// Function to display JSON data in HTML
+function displayData(data) {
+    console.log(data);
+    const container = document.getElementById('data-container');
+
+    data.forEach(item => {
+        const div = document.createElement('div');
+        div.innerHTML = `<strong>Name:</strong> ${item.name} <br>
+                         <strong>Age:</strong> ${item.age} <br>
+                         <strong>City:</strong> ${item.city} <br><br>`;
+        container.appendChild(div);
+    });
+}
+
+// Call the function to fetch and display data
+fetchData();

3113d14aa0ba071ca59cf670dd98dfd41611e386
adit <pangestuaditya.solihin@gmail.com>
2024-07-18 07:37:33 +0700

    dsfsdf

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -20,27 +20,47 @@ function getCurrentYear() {
 getCurrentYear();
 
 
-// Function to fetch and display JSON data
-function fetchData() {
-    fetch('/json/data.json') // Replace with your JSON data URL
-        .then(response => response.json())
-        .then(data => displayData(data))
-        .catch(error => console.error('Error fetching data:', error));
+const apiUrl = 'https://radical-hickory-swordtail.glitch.me';
+
+function createData() {
+    const data = document.getElementById('dataInput').value;
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: data })
+    }).then(response => response.json())
+    .then(data => console.log(data));
 }
 
-// Function to display JSON data in HTML
-function displayData(data) {
-    console.log(data);
-    const container = document.getElementById('data-container');
-
-    data.forEach(item => {
-        const div = document.createElement('div');
-        div.innerHTML = `<strong>Name:</strong> ${item.name} <br>
-                         <strong>Age:</strong> ${item.age} <br>
-                         <strong>City:</strong> ${item.city} <br><br>`;
-        container.appendChild(div);
+function readData() {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const dataList = document.getElementById('dataList');
+        dataList.innerHTML = '';
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `ID: ${item.id}, Data: ${item.data}`;
+            dataList.appendChild(li);
+        });
     });
 }
 
-// Call the function to fetch and display data
-fetchData();
+function updateData() {
+    const id = document.getElementById('updateId').value;
+    const data = document.getElementById('updateInput').value;
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: data })
+    }).then(response => response.json())
+    .then(data => console.log(data));
+}
+
+function deleteData() {
+    const id = document.getElementById('deleteId').value;
+    fetch(`${apiUrl}/${id}`, {
+        method: 'DELETE'
+    }).then(response => response.json())
+    .then(data => console.log(data));
+}
